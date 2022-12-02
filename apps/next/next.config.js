235a98c7c7ed74f030cf1ba9cffdd423f9754179
Jon Lemmon <jonlemmon@gmail.com>
2022-12-03 08:11:06 +1300

    Init

---
+++ apps/next/next.config.js
@@ -0,0 +1,44 @@
+const { withExpo } = require('@expo/next-adapter')
+const withFonts = require('next-fonts')
+const withImages = require('next-images')
+const withPlugins = require('next-compose-plugins')
+const withTM = require('next-transpile-modules')([
+  'solito',
+  'dripsy',
+  '@dripsy/core',
+  'moti',
+  'app',
+])
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // reanimated (and thus, Moti) doesn't work with strict mode currently...
+  // https://github.com/nandorojo/moti/issues/224
+  // https://github.com/necolas/react-native-web/pull/2330
+  // https://github.com/nandorojo/moti/issues/224
+  // once that gets fixed, set this back to true
+  reactStrictMode: false,
+  webpack5: true,
+  images: {
+    disableStaticImages: true,
+  },
+  experimental: {
+    forceSwcTransforms: false, // set this to true to use reanimated + swc experimentally
+    swcPlugins: [[require.resolve('./plugins/swc_plugin_reanimated.wasm')]],
+  },
+}
+
+module.exports = withPlugins(
+  [
+    withTM,
+    withFonts,
+    withImages,
+    [
+      withExpo,
+      {
+        projectRoot: __dirname + '../../..',
+      },
+    ],
+  ],
+  nextConfig
+)
